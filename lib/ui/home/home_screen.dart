47d46535313a80831cfa173d94e0dedeec831b72
Dunatron <heath.dunlop.hd@gmail.com>
2024-09-14 09:40:29 +1200

    ff

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:ants_companion/ui/ant_tiers/ant_tier_details/ant_tier_details_sliver_view.dart';
-import 'package:feedback/feedback.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
